1dc6d203cb6d764b39ff14c726e4655cb9cfe8e7
Erin Sullivan <kittylily@gmail.com>
2016-11-11 16:32:30 -0800

    ✨ Detabify and cleanup

--- butterfly-shader.fs.glsl
+++ butterfly-shader.fs.glsl
@@ -13,24 +13,24 @@ uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
 #define pi 3.141593
 #define e 2.71828
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-	vec2 p = (2.0*fragCoord-iResolution.xy)/min(iResolution.y,iResolution.x);
-	p = p * 3.0;
-	p.y += 0.75;
+  vec2 p = (2.0 * fragCoord - iResolution.xy) / min(iResolution.y, iResolution.x);
+  p = p * 3.0;
+  p.y += 0.75;
 
-	// background
-	vec3 bcol = vec3(0.0, 0.0, 0.0);
+  // background
+  vec3 bcol = vec3(0.0, 0.0, 0.0);
 
-	// shape
-	float a = atan(p.y, p.x);
-	float r = length(p);
-	float wings = sin(2.0 * a - pi) / 24.0;
-	float d = pow(e, sin(a)) - 2.0 * cos(4.0 * a) + pow(wings, 5.0);
+  // shape
+  float a = atan(p.y, p.x);
+  float r = length(p);
+  float wings = sin(2.0 * a - pi) / 24.0;
+  float d = pow(e, sin(a)) - 2.0 * cos(4.0 * a) + pow(wings, 5.0);
 
-	// color
-	vec3 hcol = vec3(1.0, 1.0, 1.0);
-	vec3 col = mix(bcol, hcol, smoothstep(-0.01, 0.01, d - r));
+  // color
+  vec3 hcol = vec3(1.0, 1.0, 1.0);
+  vec3 col = mix(bcol, hcol, smoothstep(-0.01, 0.01, d - r));
 
-	fragColor = vec4(col, 1.0);
+  fragColor = vec4(col, 1.0);
 }
